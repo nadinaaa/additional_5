8d418c1846b953c06e75206496b284796fe31a92
nadinaaa <43186448+nadinaaa@users.noreply.github.com>
2018-09-28 11:54:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opening = {
+    '[': ']',
+    '{': '}',
+    '(': ')',
+    '<': '>',
+    '|': '|',
+  };
+
+  let open = 0, close = 0;
+
+  for(let i = 0; i < str.length; i++){
+    let c = str[i];
+    if(opening[c]){
+      open++;
+    } else {
+      close++;
+    }
+
+    if(open < close){
+      return false;
+    }
+  }
+
+  if(open != close){
+    return false;
+  }
+  return true;
 }
+
