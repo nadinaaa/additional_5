02cbf43ab0c3b0edd598a19ade3f3e902b26b122
nadinaaa <43186448+nadinaaa@users.noreply.github.com>
2019-03-23 11:13:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-module.exports = function check(str, bracketsConfig) {
-  let opening = {
-    '[': ']',
-    '{': '}',
-    '(': ')',
-    '<': '>',
-    '|': '|',
-  };
-
-  let open = 0, close = 0;
-
-  for(let i = 0; i < str.length; i++){
-    let c = str[i];
-    if(opening[c]){
-      open++;
-    } else {
-      close++;
-    }
-
-    if(open < close){
-      return false;
-    }
-  }
-
-  if(open != close){
+function isEven (str) {
+  if(str.length % 2 != 0) {
     return false;
   }
   return true;
 }
+function simplify (str, config) {
+	for( var i=str.length-1; i>=0; i--) {
+		for(var j=0; j<config.length; j++) {
+			if(str.charAt(i) == config[j][0]) {
+				var same = config[j].join('');
+				str = str.replace(same,'');
+			}
+		}
+	}
+	return str;
+}
+function isEmpty (str) {
+	if(str.length == 0) {
+	return true;
+}
+return false;
+}
 
+module.exports = function check(str, bracketsConfig) {
+
+if(isEven(str) == false) {
+  return false;
+}
+str = simplify(str, bracketsConfig);
+return isEmpty(str);
+
+}
